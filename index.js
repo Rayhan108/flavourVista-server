0a5ed8ee8a2dde2782a18767fb6e8f0c9792ed80
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-04-17 23:00:49 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('FlavourVista server is running')
+})
+app.listen(port,()=>{
+    console.log(`FlavourVista serveris running on port ${port}`);
+})
